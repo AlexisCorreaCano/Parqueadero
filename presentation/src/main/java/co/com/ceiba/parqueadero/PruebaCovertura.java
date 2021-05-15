fe29cf0d0a0dba676d4a1e806058b5ac2d9928ae
alexis.correa <alexis.correa.cano@gmail.com>
2021-05-14 21:07:44 -0500

    view pager and tab layout added

--- presentation/src/main/java/co/com/ceiba/parqueadero/PruebaCovertura.java
+++
@@ -1,9 +0,0 @@
-package co.com.ceiba.parqueadero;
-
-public class PruebaCovertura {
-
-    public int sumar (int numerouno, int numerodos){
-
-        return numerouno+numerodos;
-    }
-}
---
+++ presentation/src/main/java/co/com/ceiba/parqueadero/view/adapter/ViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package co.com.ceiba.parqueadero.view.adapter;
+
+public class VieePagerAdapter {
+}
